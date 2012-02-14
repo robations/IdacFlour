4f0b955661365cf4b204909922ff10b924488c16
Robert Churchill <robert@idacmedia.com>
2012-02-14 14:13:42 +0000

    Added tableless Enquiry model for validating contact forms

---
+++ Model/Enquiry.php
@@ -0,0 +1,28 @@
+<?php
+
+class Enquiry extends AppModel
+{
+
+    public $useTable = false;
+    public $_schema = array(
+        'name' => array('type' => 'string', 'length' => 100),
+        'email' => array('type' => 'string', 'length' => 255),
+        'phone' => array('type' => 'string', 'length' => 20),
+        'message' => array('type' => 'text'),
+    );
+    public $validate = array(
+        'name' => array(
+            'rule' => array('minLength', 1),
+            'message' => 'Name is required'
+        ),
+        'email' => array(
+            'rule' => 'email',
+            'message' => 'Must be a valid email address'
+        ),
+        'message' => array(
+            'rule' => array('minLength', 1),
+            'message' => 'Please write a message'
+        ),
+    );
+
+}
